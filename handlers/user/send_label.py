8ea8e75eea999a2def7e1dfb5e5f670f922b1867
Igor Nikolayev <igors.nikos@gmail.com>
2025-07-22 12:15:37 +0300

    removed pytesseract

--- handlers/user/send_label.py
+++ handlers/user/send_label.py
@@ -7,9 +7,6 @@ from data import config
 from handlers.user.menu import *
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
-from PIL import Image
-import pytesseract
-from google import genai
 
 class SendLabelState(StatesGroup):
     label = State()
@@ -63,39 +60,3 @@ async def handle_label_expDate(message: types.Message, state: FSMContext):
     await message.answer("Label added!", reply_markup=home_user_markup())
     await state.finish()
 
-
-@dp.message_handler(content_types=ContentType.PHOTO, state=SendLabelState.label)
-async def handle_label_photo(message: types.Message, state: FSMContext):
-        
-    fileID = message.photo[-1].file_id
-    file_info = await bot.get_file(fileID)
-    downloaded_file = (await bot.download_file(file_info.file_path, os.path.join(os.getcwd(), "img.jpg")))
-
-    image = Image.open(downloaded_file.name)
-    pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-    text = pytesseract.image_to_string(image)
-
-    client = genai.Client(api_key="AIzaSyBjRZND0JalSXr1aDCQeXCM8sKndymvbWM")
-
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=f"Send me back Label number and Date(DD/MM/YYYY) in YYYY-MM-DD format without comments: {text}"
-    )
-
-    if response.text:
-        responseData = response.text.partition(' ')
-        label = responseData[0]
-        expDate = responseData[2]
-        if label.isnumeric():
-            db.query('INSERT INTO labels VALUES (?, ?, ?, ?, ?)', (None, message.from_user.id, datetime.datetime.now(), label, expDate))
-        else:
-            await message.answer("Error reading image!", reply_markup=home_user_markup())
-            await state.finish() 
-            return
-    else:
-        await message.answer("Server Busy. Try again...", reply_markup=home_user_markup())
-        await state.finish()
-        return
-
-    await message.answer(f"{label}\n{expDate}", reply_markup=home_user_markup())
-    await state.finish()
